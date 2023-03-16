ca944ffa7f8e4416c99a1bd73df049c774f3b013
Sarim13500 <sarimasher@hotmail.com>
2023-03-16 17:31:48 +0100

    Server og klient programmet kjører og kobler seg opp mot hverandre

--- simpleperf.py
+++ simpleperf.py
@@ -1,82 +1,113 @@
+import socket
 from socket import *
 import sys
 import _thread as thread
 
 input_lengde = len(sys.argv)
+
 Port = 8088
 Ip = '127.0.0.1'
 Format = "MB"
 time = 50
 
-i =1
+virker = True
+
+
+#Tomme argimenter kan løses på to måter. Bruke default eller avslutte prosess
+
+
+x = 0
+
 #While løkke som går gjennom input argumentene
-while( i < input_lengde):
+for i in sys.argv:
+
 
     # Finner porten fra input argumentene
-    if (sys.argv[i] == "-p"):
-        Port = int (sys.argv[i+1])
-        if(Port<1024):
+    if i =="-p":
+        Port = int (sys.argv[x+1])
+
+        if Port<1024:
             print("Feil! Porten stemmer ikke. Skriv inn en port som er større eller lik 1024")
-        elif(Port > 65535):
+            virker = False
+        elif Port > 65535:
             print("Feil! Porten stemmer ikke. Skriv inn en port som er mindre eller lik 65535")
+            virker = False
+
 
     # Finner foramten fra input argumentene
-    if (sys.argv[i] == "-f"):
-        Format = sys.argv[i+1]
-    ++i
+    if i == "-f":
+        Format = sys.argv[x+1]
+        if Format != "KB" and Format != "MB" and Format != "B":
+            print("Skriv inn riktig format. Enten KB, MB eller B etter -f")
+            virker = False
 
 
-#Server kode
-if (sys.argv[1] == "-s"):
-
-    i=0
-    while (i < input_lengde):
-        # Finner ip-adressen fra input argumentene
-        if (sys.argv[i] == "-b"):
-            Ip = sys.argv[i+1]
-
-
-        
-    sock = socket(AF_INET, SOCK_STREAM)
-
-    sock.bind((Ip, Port))
-    sock.listen(1)
-
-
-#Klient kode
-elif (sys.argv[1] == "-c"):
-
-    i=0
-    while (i < input_lengde):
-        # Finner ip-adressen fra input argumentene
-        if (sys.argv[i] == "-I"):
-            Ip = sys.argv[i+1]
-
-        # Finner tiden fra input argumentene
-        if (sys.argv[i] == "-t"):
-            time = int (sys.argv[i+1])
-            if(time <=0):
-                print("Tid må være større en 0")
-
-        # Finner intervallet fra input argumentene
-        if (sys.argv[i] == "-i"):
-            intervall = sys.argv[i+1]
-
-        # Finner antall paralelle klienter fra input argumentene
-        if (sys.argv[i] == "-P"):
-            paraleller= sys.argv[i + 1]
-
-        # Finner antall paralelle klienter fra input argumentene
-        if (sys.argv[i] == "-n"):
-            no_of_bytes= sys.argv[i + 1]
-            #if (no_of_bytes == ""):
+    # Finner ip-adressen fra input argumentene
+    if (i == "-b"):
+        Ip = sys.argv[x+1]
+        print(Ip)
 
 
 
+    # Finner ip-adressen fra input argumentene
+    if (i == "-I"):
+        Ip = sys.argv[x+1]
 
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((Ip, Port))
+
+    # Finner tiden fra input argumentene
+    if (i == "-t"):
+        time = int(sys.argv[x+1])
+        if (time <= 0):
+            print("Tid må være større en 0")
+
+
+    # Finner intervallet fra input argumentene
+    if (i == "-i"):
+        intervall = sys.argv[x+1]
+
+
+    # Finner antall paralelle klienter fra input argumentene
+    if (i == "-P"):
+        paraleller = sys.argv[x+1]
+
+
+    # Finner antall paralelle klienter fra input argumentene
+    if (i == "-n"):
+        no_of_bytes = sys.argv[x+1]
+        # if (no_of_bytes == ""):
+
+    x=x+1
+
+
+if virker == False:
+    print("Feil i syntax. Prøv igjen")
 
 
 else:
-    print("Error: you must run either in server or client mode")
+    #Server kode
+    if (sys.argv[1] == "-s"):
+
+
+        sock = socket(AF_INET, SOCK_STREAM)
+
+        sock.bind((Ip, Port))
+        sock.listen(5)
+
+        # mens serveren er åpen
+        while True:
+            #Klart til å betjene klient
+            print("[Server] Klar til å koble seg til ")
+
+
+
+
+
+    #Klient kode
+    elif (sys.argv[1] == "-c"):
+
+        sock = socket(AF_INET, SOCK_STREAM)
+        sock.connect((Ip, Port))
+
+
+    else:
+        print("Error: you must run either in server or client mode")
